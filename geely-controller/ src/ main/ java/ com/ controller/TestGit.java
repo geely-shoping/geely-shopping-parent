739cdb26eba7cd704dacb152a4bd11faca7968d7
lxf <1650062546@qq.com>
2020-02-28 12:17:15 +0800

    第三次test

--- geely-controller/ src/ main/ java/ com/ controller/TestGit.java
+++ geely-controller/ src/ main/ java/ com/ controller/TestGit.java
@@ -2,6 +2,6 @@ package com.controller;
 //测试git---lw111
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("sssss");
+        System.out.println("sssss杀杀杀");
     }
 }
