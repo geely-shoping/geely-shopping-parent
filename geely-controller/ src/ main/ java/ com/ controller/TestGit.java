61cb99dc07198a540fe57de70ddad74ae000a501
hzh <2603894867@qq.com>
2020-02-28 20:48:25 +0800

    黄志焕第一次提交

--- geely-controller/ src/ main/ java/ com/ controller/TestGit.java
+++ geely-controller/ src/ main/ java/ com/ controller/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("sssss哦哦哦");
         System.out.println("hhhhh1");
-        System.out.println("黄zhihuan");
+        System.out.println("黄志欢");
     }
 }
