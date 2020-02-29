e29f523cb673ee8d0f3a71ecc62f29a3569f9a5e
li wei <1484718812@qq.com>
2020-02-29 09:55:18 +0800

    黄志焕提交用户模块实体类

--- geely-entity/src/main/java/com/entity/user/AccountInfo.java
+++ geely-entity/src/main/java/com/entity/user/AccountInfo.java
@@ -7,10 +7,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+<<<<<<< Updated upstream
 public class AccountInfo {
 
     private Integer accountInfoId;
     private Integer accountId;
     private Integer InfoId;
+=======
+/**
+ * 账号信息关联表
+ */
+public class AccountInfo {
+
+    private Integer accountInfoId;  //关联ID
+    private Integer accountId;  //账号ID
+    private Integer InfoId; //信息ID
+
+
+>>>>>>> Stashed changes
 
 }
