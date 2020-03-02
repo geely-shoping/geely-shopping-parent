dc66c2df16bdca887afa83458c6746933d8baf05
lxf <1650062546@qq.com>
2020-03-02 15:21:20 +0800

    李小峰：创建VO类

--- geely-controller/ src/ main/ java/com/geely/controller/TestGit.java
+++
@@ -1,7 +0,0 @@
-package com.geely.controller;
-
-public class TestGit {
-    public static void main(String[] args) {
-        System.out.println("test Git");
-    }
-}
--- geely-controller/ src/ main/ java/com/geely/vo/ResponseVO.java
+++ geely-controller/ src/ main/ java/com/geely/vo/ResponseVO.java
@@ -1,4 +1,4 @@
-package com.geely.VO;
+package com.geely.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
