868662e2643d86d949f8708388e1eed4fc3ec40d
lxf <1650062546@qq.com>
2020-03-02 14:58:46 +0800

    李小峰：创建VO类

---
+++ geely-controller/ src/ main/ java/com/geely/controller/user/UserLoginController.java
@@ -0,0 +1,17 @@
+package com.geely.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: geely-shopping-parent
+ * @description: 用户登录控制器
+ * @author: admin
+ * @create: 2020-03-02 14:45
+ **/
+@Controller
+@ResponseBody
+public class UserLoginController {
+
+    //用户登录校验
+}
